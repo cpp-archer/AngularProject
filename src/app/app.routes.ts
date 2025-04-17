669703fc32ffb9e6dfc94641cba83dcf256cf0e4
DJAOUEL Jenna Louisa <jennalouisa.dja150@gmail.com>
2025-04-17 12:03:15 +0200

    OMGGGGGGGGG CA MARCHE

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home';
 import { UserComponent } from './user';
+import { UserListComponent } from './user-list/user-list.component';
 
 export const routes: Routes = [
-    { path: '', component : HomeComponent },
-    {path:'user', component : UserComponent},
+    // { path: '', component : HomeComponent },
+    // {path:'user', component : UserComponent}
 
+    // {path:"**", redirectTo: ''}
 
-    {path:"**", redirectTo: ''}
+    {path: 'home' , component: HomeComponent},
+    {path: 'createUser' , component: UserComponent},
+    {path: 'listUser' , component: UserListComponent},
 ];
 
